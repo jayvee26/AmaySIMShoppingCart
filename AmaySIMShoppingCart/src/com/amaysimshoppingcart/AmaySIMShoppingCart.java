4cc0c9bbedf3d60b875ba6e8989586e84e8c8b54
Jayvee <Jayvee@192.168.1.2>
2016-09-11 11:53:23 +0800

    Initial Setup

---
+++ AmaySIMShoppingCart/src/com/amaysimshoppingcart/AmaySIMShoppingCart.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.amaysimshoppingcart;
+
+/**
+ *
+ * @author Jayvee
+ * @since September 11, 2016
+ * @version 1.0
+ */
+public class AmaySIMShoppingCart {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ AmaySIMShoppingCart/src/com/amaysimshoppingcart/utilities/PropertiesUtility.java
@@ -0,0 +1,40 @@
+/*
+ * This code is a property of Amaysim.
+ * Any change to this code must have permission of the company.
+ */
+package com.amaysimshoppingcart.utilities;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+/**
+ *
+ * @author Jayvee
+ * @since September 11, 2016
+ * @version 1.0
+ */
+public class PropertiesUtility {
+    
+    /**
+     * @param propertyFileName the filename of the property file
+     * @return Properties the properties loaded from the properties file
+     * @throws IOException 
+     */
+    public static Properties loadProperties(String propertyFileName) throws IOException {
+        File file = new File(propertyFileName);
+        String absolutePath = file.getAbsolutePath();
+        Path configLocation = Paths.get(absolutePath);
+        System.out.println("Config location: " + configLocation.toString());
+        InputStream stream = Files.newInputStream(configLocation);
+        Properties properties = new Properties();
+
+        properties.load(stream);
+
+        return properties;
+    }
+}
